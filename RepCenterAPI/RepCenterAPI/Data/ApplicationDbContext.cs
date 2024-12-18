c6d5a849d49fc54c24b1555785ba1dc5fcc9e9cc
Voron <67822791+VoronTheChell@users.noreply.github.com>
2024-12-18 15:25:12 +0400

    First Mobile Concemt Commint
    Comit from 18/12/2024

---
+++ RepCenterAPI/RepCenterAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MilkDeliveryAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Studet> Students { get; set; }
+        public DbSet<Pridment> Pridments { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+    }
+}
